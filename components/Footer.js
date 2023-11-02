bd4206572a968293ef96c0234b6dcef48deadb88
lisandro cordoba <lisandrocba_7@hotmail.com>
2023-11-02 17:21:07 -0300

    cambios de estilos contacto footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,23 +5,23 @@ import logoHPampa from "../public/logo-HPampaBlanco.png"
 const Footer = () => {
   return (
     <div>
-        <div className="p-5 h-56 flex justify-around items-center">
+        <div className="p-5 h-56 flex justify-around items-center bg-slate-800">
             <div className="w-1/2">
                 <Image className="m-3" src={logoHPampa} alt="logohpampa" width={70} height={40}/>
             </div>
             <div className="flex justify-between w-1/2">
                 <div>
-                    <h3 className="text-hpampa text-sm mb-3">Nuestras redes</h3>
-                    <p className="text-blanco text-xs underline mb-1">Linkedin</p>
-                    <p className="text-blanco text-xs underline mb-1">Instagram</p>
-                    <p className="text-blanco text-xs underline mb-1">Facebook</p>
-                    <p className="text-blanco text-xs underline mb-1">Whatsapp</p>
+                    <h3 className="text-hpampa text-xl mb-3">Nuestras redes</h3>
+                    <p className="text-blanco text-lg underline mb-1">Linkedin</p>
+                    <p className="text-blanco text-lg underline mb-1">Instagram</p>
+                    <p className="text-blanco text-lg underline mb-1">Facebook</p>
+                    <p className="text-blanco text-lg underline mb-1">Whatsapp</p>
                 </div>
                 <div>
-                    <h3 className="text-hpampa text-sm mb-3">Encontranos</h3>
-                    <p className="text-blanco text-xs underline mb-1">La Muela 50196, Zaragoza, España</p>
-                    <p className="text-blanco text-xs underline mb-1">+34 606 568 570</p>
-                    <p className="text-blanco text-xs underline mb-1">consultas@hpampa.com</p>
+                    <h3 className="text-hpampa text-xl mb-3">Encontranos</h3>
+                    <p className="text-blanco text-lg underline mb-1">La Muela 50196, Zaragoza, España</p>
+                    <p className="text-blanco text-lg underline mb-1">+34 606 568 570</p>
+                    <p className="text-blanco text-lg underline mb-1">consultas@hpampa.com</p>
                 </div>
             </div>
         </div>
