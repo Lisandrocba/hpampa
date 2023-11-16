993aaa176d61ce71f8f4428e25a0dd0912b75459
lisandro cordoba <lisandrocba_7@hotmail.com>
2023-11-16 13:37:42 -0300

    terminando page frutoscecos legumbres

--- pages/frutossecos.js
+++ pages/frutossecos.js
@@ -1,3 +1,4 @@
+import CartProductos from '@/components/CartProductos'
 import NavBarSectores from '@/components/NavBarSectores'
 import React from 'react'
 
@@ -5,7 +6,42 @@ const FrutosSecos = () => {
   return (
     <div>
       <NavBarSectores />
-      FrutosSecos
+      <div className="flex justify-center items-center flex-wrap my-10">
+      <CartProductos
+          img="https://i.imgur.com/yIVrbsh.png"
+          productos={[
+            "Maní Roruka tostado sin sal linea gourmet",
+            "Maní Roruka tostado con sal linea gourmet",
+            "Té negro Ceylon con azahar",
+            "Té negro con especias y hierbas",
+            "Té rojo con chocolate & frutos rojos",
+            "Arandanos en Polvo",
+          ]}
+          black={true}
+        />
+         <CartProductos
+          img="https://i.imgur.com/5hCgg50.png"
+          productos={[
+            "Nuez Mammoth Halves",
+            "Nuez Junior Mammoth Halves",
+            "Nuez Extra large Halves",
+            "Nuez Medium Peces",
+            "Nuez Large/medium Peces",
+            "Nuez Mammoth Peces",
+          ]}
+          black={true}
+        />
+         <CartProductos
+          img="https://i.imgur.com/2Uqaimp.png"
+          productos={[
+            "Ciruelas Desecadas",
+            "Pasas de Uva",
+            "Puré de ciruelas",
+            "Pasas de Uva",
+          ]}
+          imgW={250}
+        />
+      </div>
     </div>
   )
 }
--- pages/legumbres.js
+++ pages/legumbres.js
@@ -1,13 +1,13 @@
-import CartProductos from '@/components/CartProductos'
-import NavBarSectores from '@/components/NavBarSectores'
-import React from 'react'
+import CartProductos from "@/components/CartProductos";
+import NavBarSectores from "@/components/NavBarSectores";
+import React from "react";
 
 const Legumbres = () => {
   return (
     <div>
       <NavBarSectores />
       <div className="flex justify-center items-center flex-wrap my-10">
-      <CartProductos
+        <CartProductos
           img="https://i.imgur.com/5yAI1Te.png"
           productos={[
             "Porotos Alubia",
@@ -20,11 +20,11 @@ const Legumbres = () => {
             "Porotos Adzuki",
             "Porotos Cramberry",
           ]}
-          w='w-1/2'
+          w="w-1/2"
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Legumbres
+export default Legumbres;
