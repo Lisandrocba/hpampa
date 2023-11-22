80e9c7387be3c6f54130d8d741efb6b6a5054ac3
lisandro cordoba <lisandrocba_7@hotmail.com>
2023-11-22 19:05:22 -0300

    creando arreglo de imganes productos

---
+++ data/ProdBodegas.js
@@ -0,0 +1,24 @@
+export const productos = [
+    araujo = ['https://i.imgur.com/s27W6TU.png',
+    'https://i.imgur.com/h82aHSH.png',
+    'https://i.imgur.com/e4sxUa8.png',
+    'https://i.imgur.com/VZwSl5w.png',
+    'https://i.imgur.com/vOJX06Q.png',
+    'https://i.imgur.com/TROrelS.png'],
+    aconcagua = [
+        'https://i.imgur.com/IAPx5Uq.png',
+        'https://i.imgur.com/VR3yPIT.png',
+        'https://i.imgur.com/xLorlUN.png',
+        'https://i.imgur.com/bfpMSUF.png',
+        'https://i.imgur.com/yJNY4QT.png',
+        'https://i.imgur.com/MXXw5vh.png'
+    ],
+    carelli = [
+        'https://i.imgur.com/TyuFuIH.png',
+        'https://i.imgur.com/BHB7u0Q.png',
+        'https://i.imgur.com/DDUnsqY.png',
+        'https://i.imgur.com/XGcIUap.png',
+        'https://i.imgur.com/AHWVflR.png'
+    ],
+    
+]
